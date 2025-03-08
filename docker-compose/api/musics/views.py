72bde469b76cae33fc199fd27b48af32243af34b
redatman <evicwork@gmail.com>
2025-03-08 23:45:56 +0800

    feat: initialize docker-compose(include Django project) with Dev Container configurations

---
+++ docker-compose/api/musics/views.py
@@ -0,0 +1,28 @@
+from .models import Music
+from .serializers import MusicSerializer
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class MusicViewSet(viewsets.ModelViewSet):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+    permission_classes = (IsAuthenticated,)
+    # parser_classes = (JSONParser,)
+
+    # [GET] /api/musics/{pk}/detail/
+    @action(detail=True, methods=['get'], url_path='detail')
+    def detail_action(self, request, pk=None):
+        music = get_object_or_404(Music, pk=pk)
+        result = {
+            'singer': music.singer,
+            'song': music.song
+        }
+        return Response(result, status=status.HTTP_200_OK)
+
+    # [GET] /api/musics/all_singer/
+    @action(detail=False, methods=['get'], url_path='all_singer')
+    def all_singer(self, request):
+        music = Music.objects.values_list('singer', flat=True)
